97f2cc9df35588f07b035f9cf97cacd28840f91a
cccs-nik <68961854+cccs-nik@users.noreply.github.com>
2025-08-06 17:42:46 -0400

    woops didn't mean to remove this one

--- lib/trino-filesystem-cache-alluxio/src/main/java/io/trino/filesystem/alluxio/PatternBasedCacheFilter.java
+++ lib/trino-filesystem-cache-alluxio/src/main/java/io/trino/filesystem/alluxio/PatternBasedCacheFilter.java
@@ -28,6 +28,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class PatternBasedCacheFilter
         implements CacheFilter
