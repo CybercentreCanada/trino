abbe67179ec20b38215c1e19f66f8e6304b48c22
cccs-nik <68961854+cccs-nik@users.noreply.github.com>
2025-08-13 15:14:18 -0400

    ScheduledThreadPoolExecutor import

--- lib/trino-filesystem-cache-alluxio/src/main/java/io/trino/filesystem/alluxio/AlluxioFileSystemCache.java
+++ lib/trino-filesystem-cache-alluxio/src/main/java/io/trino/filesystem/alluxio/AlluxioFileSystemCache.java
@@ -36,7 +36,7 @@ import jakarta.annotation.PreDestroy;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
