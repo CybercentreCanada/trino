8374468cc4273b0563d4f252db9be3fe4c710390
cccs-nik <68961854+cccs-nik@users.noreply.github.com>
2025-10-29 11:56:01 -0400

    removing old debugging

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/catalog/rest/TrinoRestCatalog.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/catalog/rest/TrinoRestCatalog.java
@@ -833,13 +833,13 @@ public class TrinoRestCatalog
         return switch (sessionType) {
             case NONE -> {
                 String sessionId = hashCredentials(credentials);
-                log.warn("Generated sessionId for NONE sessionType: %s", sessionId);
+                log.debug("Generated sessionId for NONE sessionType: %s", sessionId);
 
                 yield new SessionCatalog.SessionContext(sessionId, null, credentials, ImmutableMap.of(), session.getIdentity());
             }
             case USER -> {
                 String sessionId = format("%s-%s", session.getUser(), session.getSource().orElse("default"));
-                log.warn("Generated sessionId for USER sessionType: %s", sessionId);
+                log.debug("Generated sessionId for USER sessionType: %s", sessionId);
 
                 Map<String, String> properties = ImmutableMap.of(
                         "user", session.getUser(),
